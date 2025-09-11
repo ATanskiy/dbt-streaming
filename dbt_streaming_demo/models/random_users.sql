ffddc32d6418605dad57690a8391ed55257c8601
atans <atanskiy1989@gmail.com>
2025-09-11 18:41:54 +0300

    working set

--- dbt_streaming_demo/models/random_users.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-    materialized='streaming',
-    topic='random_users',
-    checkpoint='s3a://raw-from-kafka/_checkpoints/random_users',
-    target_table='default.random_users'
-) }}
-
-select * from stream_random_users
